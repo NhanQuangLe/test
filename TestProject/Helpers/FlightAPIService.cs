e6b25d37e398df4d9c694664b589053ec7983080
Le Quang Nhan <21522402@gm.uit.edu.vn>
2024-05-10 03:02:51 +0700

    finish

--- TestProject/Helpers/FlightAPIService.cs
+++ TestProject/Helpers/FlightAPIService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestProject.Model;
 
 namespace TestProject.Helpers
 {
@@ -11,7 +12,11 @@ namespace TestProject.Helpers
     {
         // TODO: Define your api service here. Ref: https://github.com/reactiveui/refit
         //Example
-        //[Get("/users/{user}")]
-        //Task<User> GetUser(string user);
+        [Get("/flights")]
+        Task<List<Flight>> GetAllFlight(string user)
+        {
+            // Em không làm phần backend call api trên trường
+            // Lúc học wpf em lấy data từ Local SQL server thôi ạ
+        }
     }
 }
---
+++ TestProject/Model/Flight.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject.Model
+{
+    internal class Flight
+    {
+        public string Id { get; set; }
+        public string Airline { get; set; }
+        public string Local_depart { get; set; }
+        public string originAirport { get; set; }
+        public string Local_arrive { get; set; }
+        public string destinationAirport { get; set; }
+    }
+}
